384e1f4da9b98bd37f9837709ed4a0b0c1663f3f
Jason Neylon <jason.neylon@uswitch.com>
2012-11-17 18:56:10 +0000

    add a recommendations hash

---
+++ recommendations.rb
@@ -0,0 +1,48 @@
+﻿$recommendations = {
+  "house" => [
+    {
+      "id" => "energy-bill",
+      "saving" => 90,
+      "recomendation" => "Solar PV panels",
+      "description"=> "Generate your own electricity and earn over £500 a year from Feed-in tariff."
+    },
+    { 
+      "id" => "energy-bill",
+      "saving"  => 175,
+      "recomendation"  => "Loft insulation",
+      "description" => "Loft insulation is one of the most effective ways of keeping heat. Plus there are many free schemes now available from energy companies."
+    },
+    { 
+      "id" => "energy-bill",
+      "saving"  => 130,
+      "recomendation"  => "Ground source heat pump",
+      "description" => "Heat your home with energy from the ground. Funding available next year via the Renewable Heat Incentive."
+    },
+    { 
+      "id" => "energy-bill",
+      "saving"  => 300,
+      "recomendation"  => "New boiler",
+      "description" => "If you have a old inefficient boiler, a new boiler can give you big savings. Funding avaialbe via the Green Deal."
+    },
+  ],
+  "flat" => [
+    { 
+      "id" => "energy-bill",
+      "saving"  => 35,
+      "recomendation"  => "Energy monitor",
+      "description" => "An energy monitor can show you which applicances are using the most electricity. Studies have shown they can reduce your electricity use by 10%."
+    },
+    { 
+      "id" => "energy-bill",
+      "saving"  => 37,
+      "recomendation"  => "Standby saver",
+      "description" => "An energy monitor can show you which applicances are using the most electricity. Studies have shown they can reduce your electricity use by 10%."
+    },
+    { 
+      "id" => "energy-bill",
+      "saving"  => 50,
+      "recomendation"  => "LED bulbs",
+      "description" => "If you have halogen downlights replacing them with more efficent LEDs can give you big savings."
+    }
+  ]
+}
