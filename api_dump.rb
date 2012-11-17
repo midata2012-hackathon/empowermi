4b29b4bf17d26e655af16c392bca041ae4074d1e
Andrzej Byczko <andrzej.byczko@dressipi.com>
2012-11-17 17:25:00 +0000

    API small cleanup

--- api_dump.rb
+++ api_dump.rb
@@ -60,40 +60,43 @@ $transaction_data_sets = %w(
 
 def pds_call(persona_id, data_sets)
   persona = $config[persona_id]
-  base_uri = "https://sbx-api.mydex.org/api/pds/pds/#{persona[:uid]}.#{RESPONSE_FORMAT}"
   opts = {
     key: persona[:key],
-    api_key: $config[:api_key],
     con_id: persona[:con_id],
     source_type: 'connection',
     dataset: data_sets.join(' ')
-
   }
-  RestClient.get(base_uri, params: opts)
+  api_call('pds', persona, opts)
 end
 
 def transaction_call(persona_id, data_sets)
   persona = $config[persona_id]
-  base_uri = "https://sbx-api.mydex.org/api/pds/transaction/#{persona[:uid]}.#{RESPONSE_FORMAT}"
   opts = { 
     key: persona[:transaction_key],
-    api_key: $config[:api_key],
     dataset: data_sets
   }
-  RestClient.get(base_uri, params: opts)
+  api_call('transaction', persona, opts)
 end
 
-def pds_data(persona_id)
-  @pds_data ||= {}
-  @pds_data[persona_id] ||= begin
+def api_call(type, persona, opts = {})
+  base_uri = "https://sbx-api.mydex.org/api/pds/#{type}/#{persona[:uid]}.#{RESPONSE_FORMAT}"
+  defaults = { 
+    api_key: $config[:api_key],
+  }
+  RestClient.get(base_uri, params: defaults.merge(opts))
+end
+
+def pds_data_cache(persona_id)
+  @pds_data_cache ||= {}
+  @pds_data_cache[persona_id] ||= begin
                    result = pds_call(persona_id, $pds_data_sets)
                    Yajl::Parser.parse(result)
                  end
 end
 
-def transaction_data(persona_id)
-  @transaction_data ||= {}
-  @transaction_data[persona_id] ||= begin
+def transaction_data_cache(persona_id)
+  @transaction_data_cache ||= {}
+  @transaction_data_cache[persona_id] ||= begin
                                       d = {}
                                       $transaction_data_sets.each do |data_set_name|
                                         result = transaction_call(persona_id, [data_set_name])
@@ -115,20 +118,31 @@ class Persona
   end
 
   def name
-    personal = pds_data(@persona_id)['field_ds_personal_details'].values.first
     "#{get(personal, 'personal_fname')} #{get(personal, 'personal_faname')}"
   end
 
   def postcode
-    home = pds_data(@persona_id)['field_ds_home'].values.first
     get(home, 'home_postcode')
   end
 
+  def property_type
+    get(home, 'home_property_type')
+  end
+
   def energy_bill
-    bills = transaction_data(@persona_id)["field_ds_utility_billing"]
+    bills = transaction_data_cache(@persona_id)["field_ds_utility_billing"]
     bills = bills.find_all { |ut| ut['field_utility_service'].to_s =~ /(electricity|gas)/i }
     bills = bills.sort { |b1, b2| Date.strptime(b1['field_utility_billing_start'], '%m/%d/%Y') <=>  Date.strptime(b2['field_utility_billing_start'], '%m/%d/%Y') }
     bills[0..11].reduce(0) { |acc, bill| acc += bill['field_utility_billing_total'].to_f }
   end
-end
 
+  private
+  def home
+    pds_data_cache(@persona_id)['field_ds_home'].values.first
+  end
+
+  def personal
+    pds_data_cache(@persona_id)['field_ds_personal_details'].values.first
+  end
+end
+#load 'api_dump.rb' ; p = Persona.new(:rbfish)
