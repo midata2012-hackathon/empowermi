ebe2683a015f15fb15a3673b5ea58168452218ae
Andrzej Byczko <andrzej.byczko@dressipi.com>
2012-11-18 01:00:22 +0000

    added presentation colours to recommendations

--- recommendations.rb
+++ recommendations.rb
@@ -3,46 +3,53 @@
     {
       "id" => "energy-bill",
       "saving" => 90,
-      "recomendation" => "Solar PV panels",
-      "description"=> "Generate your own electricity and earn over £500 a year from Feed-in tariff."
+      "recommendation" => "Solar PV panels",
+      "description"=> "Generate your own electricity and earn over £500 a year from Feed-in tariff.",
+      "color" => "#FE67EB"
     },
     { 
       "id" => "energy-bill",
       "saving"  => 175,
-      "recomendation"  => "Loft insulation",
-      "description" => "Loft insulation is one of the most effective ways of keeping heat. Plus there are many free schemes now available from energy companies."
+      "recommendation"  => "Loft insulation",
+      "description" => "Loft insulation is one of the most effective ways of keeping heat. Plus there are many free schemes now available from energy companies.",
+      "color" => "#9B4EE9"
     },
     { 
       "id" => "energy-bill",
       "saving"  => 130,
-      "recomendation"  => "Ground source heat pump",
-      "description" => "Heat your home with energy from the ground. Funding available next year via the Renewable Heat Incentive."
+      "recommendation"  => "Ground source heat pump",
+      "description" => "Heat your home with energy from the ground. Funding available next year via the Renewable Heat Incentive.",
+      "color" => "#24E0FB"
     },
     { 
       "id" => "energy-bill",
       "saving"  => 300,
-      "recomendation"  => "New boiler",
-      "description" => "If you have a old inefficient boiler, a new boiler can give you big savings. Funding avaialbe via the Green Deal."
+      "recommendation"  => "New boiler",
+      "description" => "If you have a old inefficient boiler, a new boiler can give you big savings. Funding avaialbe via the Green Deal.",
+      "color" => "#36F200"
     },
   ],
   "flat" => [
     { 
       "id" => "energy-bill",
       "saving"  => 35,
-      "recomendation"  => "Energy monitor",
-      "description" => "An energy monitor can show you which applicances are using the most electricity. Studies have shown they can reduce your electricity use by 10%."
+      "recommendation"  => "Energy monitor",
+      "description" => "An energy monitor can show you which applicances are using the most electricity. Studies have shown they can reduce your electricity use by 10%.",
+      "color" => "#FFBE28"
     },
     { 
       "id" => "energy-bill",
       "saving"  => 37,
-      "recomendation"  => "Standby saver",
-      "description" => "An energy monitor can show you which applicances are using the most electricity. Studies have shown they can reduce your electricity use by 10%."
+      "recommendation"  => "Standby saver",
+      "description" => "An energy monitor can show you which applicances are using the most electricity. Studies have shown they can reduce your electricity use by 10%.",
+      "color" => "#D29680"
     },
     { 
       "id" => "energy-bill",
       "saving"  => 50,
-      "recomendation"  => "LED bulbs",
-      "description" => "If you have halogen downlights replacing them with more efficent LEDs can give you big savings."
+      "recommendation"  => "LED bulbs",
+      "description" => "If you have halogen downlights replacing them with more efficent LEDs can give you big savings.",
+      "color" => "#FF5353"
     }
   ]
 }
